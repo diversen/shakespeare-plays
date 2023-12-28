f7df333fae974634e70afbfc04dc94208b8ce165
diversen <dennis.iversen@gmail.com>
2023-12-28 12:06:10 +0100

    script for generating new html files

---
+++ bin/generate_html.py
@@ -0,0 +1,21 @@
+"""
+Script that generates html files from the csv files
+These are saved to docs/
+
+"""
+import json
+import os
+
+# read plays_meta.json
+with open("./plays_meta.json", "r") as f:
+    plays_meta = json.load(f)
+
+    for play_meta in plays_meta:
+        name = play_meta["name"]
+        file_name = play_meta["file_name"]
+        command = f'pandoc -s md/{file_name}.md -o docs/{file_name}.html --metadata title="{name}"'
+
+        
+
+
+# print(plays_meta)
